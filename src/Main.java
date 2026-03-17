828abfab3c070aa7516735791f2512ace11ed64a
Reet2312 <reet.kathuria23@gmail.com>
2026-03-17 22:40:03 +0530

    uc11:Concurrent Booking Simulation

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
-public class UseCase10BookingCancellation {
+public class UseCase11ConcurrentBookingSimulation {
 
     public static void main(String[] args) {
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
         RoomInventory inventory = new RoomInventory();
-        CancellationService cancellationService = new CancellationService();
+        RoomAllocationService allocationService = new RoomAllocationService();
 
-        // 1. Register some confirmed bookings
-        cancellationService.registerBooking("RES-7001", "Double");
-        cancellationService.registerBooking("RES-7002", "Suite");
-        cancellationService.registerBooking("RES-7003", "Single");
+        bookingQueue.enqueueRequest(new BookingRequest("User A", "Suite"));
+        bookingQueue.enqueueRequest(new BookingRequest("User B", "Suite"));
+        bookingQueue.enqueueRequest(new BookingRequest("User C", "Single"));
+        bookingQueue.enqueueRequest(new BookingRequest("User D", "Double"));
 
-        System.out.println("Initial Inventory (Suite): " + inventory.getAvailableRooms().get("Suite"));
+        Thread t1 = new Thread(
+                new ConcurrentBookingProcessor(bookingQueue, inventory, allocationService)
+        );
 
-        // 2. Perform cancellations
-        cancellationService.cancelBooking("RES-7002", inventory); // Suite
-        cancellationService.cancelBooking("RES-7003", inventory); // Single
-        // 3. Verify Inventory Rollback
-        System.out.println("Updated Inventory (Suite): " + inventory.getAvailableRooms().get("Suite"));
+        Thread t2 = new Thread(
+                new ConcurrentBookingProcessor(bookingQueue, inventory, allocationService)
+        );
 
-        // 4. Visualize Rollback Order (LIFO)
-        cancellationService.showRollbackHistory();
+        t1.start();
+        t2.start();
 
-        // 5. Attempt invalid cancellation
-        cancellationService.cancelBooking("RES-9999", inventory);
+        try {
+            t1.join();
+            t2.join();
+            System.out.println("Concurrent processing complete.");
+        } catch (InterruptedException e) {
+            System.out.println("Thread execution interrupted.");
+        }
     }
 }
